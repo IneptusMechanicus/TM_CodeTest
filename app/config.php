d1fb112f52c9572f1c796574cb4cbf83c069b748
Reaper9806 <ivo.gerginov9806@gmail.com>
2020-11-30 03:42:11 +0200

    Config Fix

---
+++ app/config.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+Configuration variables for the database connection using pg_connect.
+Change according to your PostgreSQL setup.
+DB_HOST - Database host
+DB_USER - Database user
+DB_PASS - User password
+DB_NAME - Name of the database
+*/
+define('DB_HOST', 'localhost');
+define('DB_USER', 'ivo');
+define('DB_PASS', '1234pass');
+define('DB_NAME', 'codetest');
